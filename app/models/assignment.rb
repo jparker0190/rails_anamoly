d81ce27dd49fe41fc854433e07cbe684d70bf860
Joseph Parker <jparker0190@gmail.com>
2018-11-29 20:43:08 -0500

    updating the validates in models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,5 @@
 class Assignment < ApplicationRecord
+ validates :description, presence: true
  belongs_to :stock
  belongs_to :portfolio
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
- validates :ticker, presence: true, uniqueness: true
+ validates :ticker, presence: true, uniqueness: true, :case_sensitive => false
  validates :price, presence: true 
  has_many :assignments
  has_many :portfolios, through: :assignments
