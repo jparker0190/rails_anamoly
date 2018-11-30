b103c10ce15eef21f69e162c07e1fa9583abdcb4
Joseph Parker <jparker0190@gmail.com>
2018-11-29 19:55:02 -0500

    adding validation to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+ validates :ticker, presence: true, uniqueness: true
+ validates :price, presence: true 
  has_many :assignments
  has_many :portfolios, through: :assignments
 end
