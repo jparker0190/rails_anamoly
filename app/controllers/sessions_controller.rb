cf5eb6ed081838d51bcef01291d166be06d73aa9
Joseph Parker <jparker0190@gmail.com>
2018-11-29 20:22:52 -0500

    getting rid of unused actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,6 @@ class SessionsController < ApplicationController
  def home
  end
 
- def new
-     @user = User.new
-     render 'login'
- end
-
  # only called when signing in with Facebook
  def facebook
      @user = User.find_or_create_by(uid: auth['uid']) do |c|
@@ -28,32 +23,9 @@ class SessionsController < ApplicationController
      end
  end
 
- def login
-     @user = User.find_by(name: params[:user][:name])
-     if @user && @user.authenticate(params[:user][:password])
-         session[:user_id] = @user.id
-         redirect_to home_path
-     else
-         redirect_to login_path
-     end
- end
- 
- def logout
-     if session[:user_id]
-         session.clear
-         redirect_to root_path
-     else
-         redirect_to home_path
-     end
- end
-
  private
 
  def auth
      request.env['omniauth.auth']
- end
-
- def set_user
-     @user = User.find_by(:id => session[:user_id])
  end
 end
