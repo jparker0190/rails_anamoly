37bbd71186448f046e5b8ba6f77171bf1cd82435
Joseph Parker <jparker0190@gmail.com>
2018-11-29 21:52:16 -0500

    adding alphabetize method

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,7 @@ class Assignment < ApplicationRecord
  validates :description, presence: true
  belongs_to :stock
  belongs_to :portfolio
+ def self.alphabatize
+  @stocks = Stock.order('ticker ASC')
+ end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,16 @@
 class Stock < ApplicationRecord
+ before_save :capitalize_ticker
  validates_uniqueness_of :ticker, :case_sensitive => false
  validates :ticker, presence: true
  validates :price, presence: true 
  has_many :assignments
  has_many :portfolios, through: :assignments
+
+ def self.alphabatize
+  @stocks = Stock.order('ticker ASC')
+ end
+ def capitalize_ticker
+ self.ticker =  self.ticker.upcase
+ end
+
 end
