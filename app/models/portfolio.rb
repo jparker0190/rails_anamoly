a4ccaa092a499b17e47f2270834ed4121c271de0
Joseph Parker <jparker0190@gmail.com>
2018-11-29 20:56:31 -0500

    adding validation to portfolio name attribute

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
  validates :name, presence: true
+ validates_uniqueness_of :name, :case_sensitive => false
  belongs_to :user
  has_many :assignments
  has_many :stocks, through: :assignments
